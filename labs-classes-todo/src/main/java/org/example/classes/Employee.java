32cba784913ac1739417b9c3b13374f9b5274dcf
Michelle Tambi <michelletambi22@gmail.com>
2022-04-30 21:19:19 +0100

    created Employee class and passed Employee Test

---
+++ labs-classes-todo/src/main/java/org/example/classes/Employee.java
@@ -0,0 +1,40 @@
+package org.example.classes;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    //constructor
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    //behaviours
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+}
